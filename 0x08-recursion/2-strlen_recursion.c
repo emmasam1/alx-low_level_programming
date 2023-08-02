d95600f9d2046b2c0b69a3c83020819edb71e4c4
Emmanuel <findm4@gmail.com>
2023-08-02 13:13:27 +0100

    hot fix task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,22 +1,19 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * _strlen_recursion - Calculate the length of a string recursively.
- * @s: Pointer to the input string.
+ * _strlen_recursion - Returns the length of a string.
+ * @s: The string to be measured.
  *
  * Return: The length of the string.
  */
 int _strlen_recursion(char *s)
 {
-if (*s == '\0')
-{
-/* Base case: when we reach the null terminator, the length is 0. */
-return 0;
-}
-else
-{
-/* Recursive case: increment the length and move to the next character. */
-return 1 + _strlen_recursion(s + 1);
-}
+	int longit = 0;
+
+	if (*s)
+	{
+		longit++;
+		longit += _strlen_recursion(s + 1);
+	}
+
+	return (longit);
 }
