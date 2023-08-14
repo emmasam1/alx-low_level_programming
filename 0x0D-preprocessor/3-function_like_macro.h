f56eafb67b47274980808a11b6d6ad89eb6213bb
Emmanuel <findm4@gmail.com>
2023-08-14 13:37:04 +0100

    0x0D-preprocessor task done

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef ABS_MACRO_H
+#define ABS_MACRO_H
+
+/**
+ * ABS - Computes the absolute value of a number.
+ * @x: The number for which to compute the absolute value.
+ *
+ * Return: The absolute value of x.
+ */
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+#endif /* ABS_MACRO_H */
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_MACRO_H
+#define SUM_MACRO_H
+
+/**
+ * SUM - Computes the sum of two numbers.
+ * @x: The first number.
+ * @y: The second number.
+ *
+ * Return: The sum of x and y.
+ */
+#define SUM(x, y) ((x) + (y))
+
+#endif /* SUM_MACRO_H */
