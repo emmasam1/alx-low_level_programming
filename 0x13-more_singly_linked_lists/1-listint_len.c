1109aa6d03aad13562c2b7b6d30510982eb3aef9
Emmanuel <findm4@gmail.com>
2023-08-28 16:34:42 +0100

    task done

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - Counts the number of elements in a listint_t list.
+ * @h: Pointer to the head of the list.
+ * Return: The number of elements.
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
